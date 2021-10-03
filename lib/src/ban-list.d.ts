3f52ef7cd3eed6d136323a7279e21bf0a6d0b47d
Jensen <jensenmourat@gmail.com>
2021-10-03 11:31:54 +0400

    v1.0.5

--- lib/src/ban-list.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const KEYWORDS: readonly ["abstract", "arguments", "boolean", "break", "byte", "case", "catch", "char", "const", "continue", "debugger", "default", "delete", "do", "double", "else", "eval", "false", "final", "finally", "float", "for", "function", "goto", "if", "implements", "in", "instanceof", "int", "interface", "let", "long", "native", "new", "null", "package", "private", "protected", "public", "return", "short", "static", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "typeof", "var", "void", "volatile", "while", "with", "yield", "class", "enum", "export", "extends", "import", "super"];
-export declare type KeywordType = (typeof KEYWORDS)[number];
-export declare const BAN_LIST: string[];
